95d0dbcf276dcb8579360c7190ff2ba8db5f05b5
David <david_gakovski@outlook.com>
2021-09-01 02:07:46 +0200

    Project setup

---
+++ pet-catalog/src/test/java/mk/ukim/finki/emt/petcatalog/PetCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.petcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
