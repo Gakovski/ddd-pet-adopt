cdbb701075406aa86d8e915ef3bc704563e59e5a
David <david_gakovski@outlook.com>
2021-09-09 22:07:22 +0200

    Frontend setup part 8 - Minor changes.

--- frontend/src/components/Orders/OrderAdd/orderAdd.js
+++ frontend/src/components/Orders/OrderAdd/orderAdd.js
@@ -7,7 +7,7 @@ function OrderAdd() {
     // setTimeout may cause an error
     setTimeout(() => {
         history.push('/pets')
-    }, 7000)
+    }, 4000)
     return <div>
         <img style={{"width":"100%", "height":"100%"}} src="https://cdn.dribbble.com/users/1782673/screenshots/4683964/ezgif.com-video-to-gif__2_.gif" />
         Order Successfully Submitted
